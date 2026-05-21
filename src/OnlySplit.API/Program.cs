0ae18d2d673d18b7b1d237547eb8b378254bfa67
adirathod1822 <adirathod1822@gmail.com>
2026-05-21 23:50:07 +0530

    checking redis session connection

--- src/OnlySplit.API/Program.cs
+++ src/OnlySplit.API/Program.cs
@@ -2,6 +2,7 @@ using OnlySplit.Application;
 using OnlySplit.API.Extensions;
 using OnlySplit.Infrastructure;
 using OnlySplit.Shared;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,9 +12,20 @@ builder.Services
     .AddInfrastructure(builder.Configuration)
     .AddPresentation(builder.Configuration);
 
+builder.Services.AddSingleton<IConnectionMultiplexer>(_ =>
+{
+    var connectionString = builder.Configuration["Redis:ConnectionString"]
+        ?? throw new InvalidOperationException(
+            "Redis connection string is missing."
+        );
+
+    return ConnectionMultiplexer.Connect(connectionString);
+});
+
 var app = builder.Build();
 
 await app.BootstrapDatabaseAsync();
+
 app.UseOnlySplitPipeline();
 
 await app.RunAsync();
