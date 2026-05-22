d809cf82a3508fe20cf4ef661b41d4e7ec29dfe5
root <root@mail.onlylabs.in>
2026-05-22 09:35:19 +0530

    Revert "checking redis session connection"
    This reverts commit 2ac41db8344c03f48baa51cf0f0e5ba8b340021f.

--- src/OnlySplit.API/Program.cs
+++ src/OnlySplit.API/Program.cs
@@ -2,7 +2,6 @@ using OnlySplit.Application;
 using OnlySplit.API.Extensions;
 using OnlySplit.Infrastructure;
 using OnlySplit.Shared;
-using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,20 +11,9 @@ builder.Services
     .AddInfrastructure(builder.Configuration)
     .AddPresentation(builder.Configuration);
 
-builder.Services.AddSingleton<IConnectionMultiplexer>(_ =>
-{
-    var connectionString = builder.Configuration["Redis:ConnectionString"]
-        ?? throw new InvalidOperationException(
-            "Redis connection string is missing."
-        );
-
-    return ConnectionMultiplexer.Connect(connectionString);
-});
-
 var app = builder.Build();
 
 await app.BootstrapDatabaseAsync();
-
 app.UseOnlySplitPipeline();
 
 await app.RunAsync();
